a639d7f2355b82ff46de5c7cc8ec21dc17c42c7a
Mark Howe <mvahowe@gmail.com>
2025-03-12 14:03:58 +0100

    UI for saving

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,94 @@
-import { useState, useCallback, useEffect } from 'react';
-import { Grid2 } from "@mui/material";
-import { JsonEditor } from 'json-edit-react'
-import { getAndSetJson } from "pithekos-lib";
+import {useState, useCallback, useEffect} from 'react';
+import {Grid2, Button} from "@mui/material";
+import {JsonEditor} from 'json-edit-react'
+import {getAndSetJson, getJson} from "pithekos-lib";
 
 function App() {
-  const [i18nData, setI18nData] = useState({});
-  const [fontClass, setFontClass] = useState([]);
-  const [maxWindowHeight, setMaxWindowHeight] = useState(window.innerHeight - 64);
+    const [i18nData, setI18nData] = useState({});
+    const [unsavedData, setUnsavedData] = useState(false);
+    const [fontClass, setFontClass] = useState([]);
+    const [maxWindowHeight, setMaxWindowHeight] = useState(window.innerHeight - 64);
 
-  const handleWindowResize = useCallback(event => {
-    setMaxWindowHeight(window.innerHeight - 64);
-  }, []);
+    const handleWindowResize = useCallback(event => {
+        setMaxWindowHeight(window.innerHeight - 64);
+    }, []);
 
-  const doFetchI18n = () => {
-      getAndSetJson({
-          url: "/i18n/raw",
-          setter: setI18nData
-      })
-  }
-
-  useEffect(
-      () => doFetchI18n(),
-      []
-  );
-
-  useEffect(
-    () => {
+    const doFetchI18n = () => {
         getAndSetJson({
-            url: "/settings/typography",
-            setter: setFontClass
-        }).then()},
-    []
-  );
+            url: "/i18n/raw",
+            setter: setI18nData
+        })
+    }
 
-  useEffect(() => {
-    window.addEventListener('resize', handleWindowResize);
-    return () => {
-        window.removeEventListener('resize', handleWindowResize);
-    };
-  }, [handleWindowResize]);
+    useEffect(
+        () => doFetchI18n(),
+        []
+    );
 
-  const panksomsiaTheme = {
-    styles: {
-      container: {
-        backgroundColor: '',
-        fontFamily: '',
-      },
-      string: 'purple',
-    },
-  }
+    useEffect(
+        () => {
+            getAndSetJson({
+                url: "/settings/typography",
+                setter: setFontClass
+            }).then()
+        },
+        []
+    );
 
-  const restrictEditFilter = ({ value }) => typeof value !== "string";
-  const restrictAddFilter = ({ value }) => value && ((typeof value !== "object") || Object.values(value).filter(o => typeof o !== "string").length > 0);
-  const restrictDeleteFilter = ({ value, parentData }) => (typeof value !== "string") || (Object.keys(parentData).length === 1);
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, [handleWindowResize]);
 
-  return <Grid2 container spacing={2} sx={{ maxHeight: maxWindowHeight }} className={fontClass.font_set}>
-      <Grid2 size={12}>
-          <JsonEditor
-            data={ i18nData }
-            setData={ setI18nData }
-            theme={panksomsiaTheme}
-            rootName="i18n"
-            showStringQuotes={false}
-            restrictDrag={true}
-            restrictEdit = { restrictEditFilter }
-            restrictAdd = { restrictAddFilter }
-            restrictDelete = { restrictDeleteFilter }
-            restrictTypeSelection={["string"]}
-            defaultValue = "???"
-          />
-      </Grid2>
-  </Grid2>
+    const panksomsiaTheme = {
+        styles: {
+            container: {
+                backgroundColor: '',
+                fontFamily: '',
+            },
+            string: 'purple',
+        },
+    }
+
+    const restrictEditFilter = ({value}) => typeof value !== "string";
+    const restrictAddFilter = ({value}) => value && ((typeof value !== "object") || Object.values(value).filter(o => typeof o !== "string").length > 0);
+    const restrictDeleteFilter = ({
+                                      value,
+                                      parentData
+                                  }) => (typeof value !== "string") || (Object.keys(parentData).length === 1);
+
+    return <Grid2 container spacing={2} sx={{maxHeight: maxWindowHeight}} className={fontClass.font_set}>
+        <Grid2 item size={12}>
+            <Button
+                variant="contained"
+                disabled={!unsavedData}
+                onClick={
+                    () => {
+                        getJson("/").then(r => setUnsavedData(false));
+                    }
+                }>
+                Save
+            </Button>
+        </Grid2>
+        <Grid2 item size={12}>
+            <JsonEditor
+                data={i18nData}
+                setData={setI18nData}
+                theme={panksomsiaTheme}
+                rootName="i18n"
+                showStringQuotes={false}
+                onUpdate={() => setUnsavedData(true)}
+                restrictDrag={true}
+                restrictEdit={restrictEditFilter}
+                restrictAdd={restrictAddFilter}
+                restrictDelete={restrictDeleteFilter}
+                restrictTypeSelection={["string"]}
+                defaultValue="???"
+            />
+        </Grid2>
+    </Grid2>
 }
 
 export default App;
